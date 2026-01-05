8c0424e55aaa92ce4a69f031023f3221a7d906c2
AhmedSamir <newsamra53@gmail.com>
2026-01-05 20:25:25 +0200

    deploying on vercel

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,10 +1,9 @@
 //src/app/template.tsx
 
 'use client';
-import { ReactLenis } from 'lenis/react';
 
 const Template = ({ children }: { children: React.ReactNode }) => {
-  return <ReactLenis root>{children}</ReactLenis>;
+  return <>{children}</>;
 };
 
 export default Template;
